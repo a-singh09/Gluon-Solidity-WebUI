ebed54e0e87edd612d4e9693ff7bed7d18de90a6
Anshdeep Singh <a_singh09@yahoo.com>
2024-11-20 11:14:07 +0530

    [Fix] Resolved errors during build
    For page metadata in Next.js 13+, we need to use the metadata export or the generateMetadata function intead of directly exporting a description variable from a page component.

--- src/app/(root)/create/deployment/page.tsx
+++ src/app/(root)/create/deployment/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import FormCard from "@/components/form-card";
 
-export const description =
-  "A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings.";
-
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
