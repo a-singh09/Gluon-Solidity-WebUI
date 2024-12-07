08531dfad079c855c93472d89d64a6774aef4001
a-singh09 <a_singh09@yahoo.com>
2024-12-07 22:00:50 +0530

    [Fix] Allow images from Cloudinary by adding domain to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '/Gluon-Solidity-WebUI',
   images: {
+    domains: ["res.cloudinary.com"],
     loader: "custom",
     loaderFile: "src/loaders/cloudinary-loader.ts",
   },
