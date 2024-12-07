db168c497ddf5f0a160ba7207fe2d60700427189
a-singh09 <a_singh09@yahoo.com>
2024-12-07 21:39:02 +0530

    [Fix] Update next.config.mjs to set basePath for Gluon-Solidity-WebUI

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: '/Gluon-Solidity-WebUI',
   images: {
     loader: "custom",
     loaderFile: "src/loaders/cloudinary-loader.ts",
